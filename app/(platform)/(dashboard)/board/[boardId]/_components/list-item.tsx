067331cb94fc35962dd4468cca6073fc58b29024
Nguyen Hien <hienkoi05@gmail.com>
2025-08-09 17:35:09 +0700

    continue

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,12 +1,35 @@
-import { ListWithCards } from "@/types"
+import { ListWithCards } from "@/types";
+import { Draggable } from "@hello-pangea/dnd";
+import { ElementRef, useRef, useState } from "react";
 
 interface ListItemProps {
   data: ListWithCards;
-  index: number
+  index: number;
 }
 
-export const ListItem = ({data, index}: ListItemProps) => {
-  return (
-    <div>ListItem</div>
-  )
-}
+export const ListItem = ({ data, index }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+      textareaRef.current?.select();
+    });
+  };
+
+  return <Draggable draggableId={data.id} index={index}>
+    {provider => (
+      <li {...provider.draggableProps} ref={provider.innerRef} className="h-full w-[272]px shrink-0 select-none">
+        <div {...provider.dragHandleProps}>
+
+        </div>
+      </li>
+    )}
+  </Draggable>;
+};
